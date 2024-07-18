43b088125d078f8445b20c13e30e5163040d30db
Krishna Raut <darshanraut248@gmail.com>
2024-07-18 23:22:03 +0530

    function

---
+++ ep-05/function.js
@@ -0,0 +1,20 @@
+//function declaration
+function sum(num1, num2) {
+    console.log(num1+num2);
+}
+
+sum(100, 200); //call
+
+
+// Function expression
+
+const sumofTwoNumber = function (num1, num2) {
+    console.log(num1+num2);
+};
+
+sumofTwoNumber(1,2)
+
+
+//interview mostly asked queston -
+// what is anonymus function - 
+//a function without name that is called a function.
