4fd5246f82b2d4195f0fe48ea6b3a967c97ba64b
Krishna Raut <darshanraut248@gmail.com>
2024-07-20 00:14:08 +0530

    callbackfunct

--- ep-05/function.js
+++ ep-05/function.js
@@ -15,6 +15,21 @@ const sumofTwoNumber = function (num1, num2) {
 sumofTwoNumber(1,2)
 
 
+
+//
+
+const doSomething = function(callback) {
+    console.log("samose ban rahe hai...");
+    setTimeout(function(){callback();
+    }, 3000);
+}
+
+const callback = function() {
+    console.log("samose ready hai");
+}
+
+doSomething(callback);
+
 //interview mostly asked queston -
 // what is anonymus function - 
 //a function without name that is called a function.
