a6b94b8d87a47b4f540e12cd3d3c2b7353c0024c
Krishna Raut <darshanraut248@gmail.com>
2024-07-22 23:18:05 +0530

    array

--- ep-06/index.js
+++ ep-06/index.js
@@ -14,3 +14,21 @@ const newArr = arr.map(function(element){
 })
 
 console.log(newArray);
+
+
+//Array (map, filter, reduce)
+
+const arr = [10,20,30, "Hello"];
+
+for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    console.log(element);
+}
+
+//map
+
+const newArr = arr.map(function(element){
+    return element * 2;
+})
+
+console.log(newArray);
