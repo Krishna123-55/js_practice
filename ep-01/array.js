c1392dae4e17f52ccd2408ed6220e81e09b2b0b1
Krishna Raut <darshanraut248@gmail.com>
2024-07-24 09:41:39 +0530

    js practice

---
+++ ep-01/array.js
@@ -0,0 +1,33 @@
+// let Krishna = [112, 22, 44, 46, 768, 42];
+// Krishna.length;
+// Krishna.push(287900970);
+// Krishna.pop();
+// Krishna.unshift(1698669);
+// console.log(Krishna);
+
+
+
+// 2.Create an array with at least 5 elements. Add an element to the end of the array.
+const array = [3, 4,5 ,6,7]
+array.push(6698)
+console.log(array)
+// Remove an element from the array created earlier.
+array.pop(6698)
+console.log(array)
+
+
+
+
+
+// Object-----
+// 3. Create an object that represents a person with properties like name, age, and gender. 
+const info = {
+    name : "Krishna",
+    age : 20,
+    gender : "male"
+}
+
+console.log(info)
+//update that age
+info.age = 21;
+console.log(info)
